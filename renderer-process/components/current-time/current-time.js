d128b2df12b975ae64a9e8e8dfe07d7d85a9bac3
Arija <arija.volkova@gmail.com>
2021-06-13 19:39:48 +0200

    du-5

---
+++ renderer-process/components/current-time/current-time.js
@@ -0,0 +1,42 @@
+export class CurrentTime extends HTMLElement {
+
+    constructor() {
+        super();
+        this.innerHTML = `
+        <div id="modal-timer" class="hide">       
+        </div> 
+        `;
+
+        this.timeWord = "time";
+        this.nextPosition = 0;
+
+        document.addEventListener('keydown', (event) => {
+            if (event.key == "t" && this.nextPosition == 1) {
+                return;
+            }
+            if (event.key == this.timeWord[this.nextPosition]) {
+                this.nextPosition++;
+            } else {
+                this.nextPosition = 0;
+            }
+            if (this.nextPosition == this.timeWord.length) {
+                this.showTime();
+                this.nextPosition = 0;
+            }
+        });
+    }
+
+    showTime() {
+        document.querySelector('#modal-timer').classList.remove('hide');
+        for (var i = 0; i < 6; i++) {
+            setTimeout(function () {
+                document.querySelector('#modal-timer').innerText = (new Date()).toLocaleTimeString();
+            }, i * 1000);            
+        }
+        setTimeout(function () {
+            document.querySelector('#modal-timer').classList.add('hide');
+        }, 6000);
+    }
+}
+
+customElements.define('current-time', CurrentTime);
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,6 +1,7 @@
 import { Carousel } from './components/carousel/carousel.js';
-import { NewsArticle } from './components/news-article/news-article.js';
+//import { NewsArticle } from './components/news-article/news-article.js';
 import { Day } from './components/day/day.js';
+import { CurrentTime } from './components/current-time/current-time.js';
 
 
 /*const header = document.querySelector('header.header-news > div.header-news__container');
@@ -10,7 +11,12 @@ const carouselItemCount = 2;
 let carouselItemStart = 0;
 let articles;
 */
+const body = document.querySelector('body');
+body.appendChild(new CurrentTime());
+
 const carousel = document.querySelector('app-carousel');
+
+
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
@@ -21,9 +27,52 @@ fetch('http://localhost:3000/news.json')
 
 
 const mainContent = document.querySelector('section.main-content');
-for (let i = 0; i < 31; i++) {
-    mainContent.appendChild(new Day(i));
+
+const currentDate = new Date();
+const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+for (let i = 1; i <= maxDate; i++) {
+    const dayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
+    mainContent.appendChild(new Day(dayDate));
 }
+
+const openModalButton = document.querySelector('#open-modal');
+openModalButton.addEventListener('click', () => {
+    showDayModal().then((result) => console.log(result));
+});
+
+function showDayModal() {
+    const promiseResult = new Promise((resolve, reject) => {
+        const template = document.querySelector('#modal-template');
+        const modal = template.content.cloneNode(true);
+        const closeAction = () => {
+            const child = document.querySelector('section.modal-container');
+            document.body.removeChild(child);
+            resolve(null);
+        };
+        modal.querySelector('#close-modal').addEventListener('click', closeAction);
+        modal.querySelector('#cancel-button').addEventListener('click', closeAction);
+        modal.querySelector('#save-button').addEventListener('click', () => {
+            const formRef = document.querySelector('#modal-form');
+            const formData = new FormData(formRef);
+            const isHoliday = formData.get('isHolidayControl') === 'on';
+            resolve('ahoj!!!!!');
+        });
+        document.body.appendChild(modal);
+    });
+    return promiseResult;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 function populateNewsCarousel(news, startAt) {
