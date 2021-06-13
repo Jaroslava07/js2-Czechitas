591fe59c1ec152c2072ee61802428417e6b4ae63
Arija <arija.volkova@gmail.com>
2021-06-13 19:39:28 +0200

    lecture 8 and 9

--- renderer-process/components/day/day.js
+++ renderer-process/components/day/day.js
@@ -1,17 +1,40 @@
 export class Day extends HTMLElement {
-    constructor(dayNumber) {
-      super();
-      this.innerText = 'Den';
-      this.number = dayNumber;
-      this.addEventListener('click', this.handleClickEvent);
-     
-    }
+  constructor(date) {
+    super();
+    this.innerHTML = `
+      <div id="day-number"></div> 
+      `;
+    this.date = date;
+    this.number = date.getDate();
+    this.querySelector('#day-number').innerText = this.number;
+    this.addEventListener('click', this.handleClickEvent);
+  }
 
-    handleClickEvent () {
-      alert('clecked day: ' + this.number);
+  getDayName() {
+    switch (this.date.getDay()) {
+      case 0:
+        return 'Nedele';
+      case 1:
+        return 'Pondeli';
+      case 2:
+        return 'Utery';
+      case 3:
+        return 'Streda';
+      case 4:
+        return 'Ctvrtek';
+      case 5:
+        return 'Patek';
+      case 6:
+        return 'Sobota';
     }
-  
-    }
+  }
 
-    // сообщим браузеру, что <my-element> обслуживается нашим новым классом
+
+  handleClickEvent() {
+    alert('Dnesni den: ' + this.getDayName());
+  }
+
+}
+
+// сообщим браузеру, что <my-element> обслуживается нашим новым классом
 customElements.define('app-day', Day);
